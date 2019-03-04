1499606e9c821f0620fbb881502ce5d8f3d10bfd
tomasbrazas97 <tomasbrazas@gmail.com>
2019-03-04 23:43:20 +0000

    Respawn
    Respawn points added to work with checkpoints

--- Dragonball Switch/Assets/Scripts/Player_Move.cs
+++ Dragonball Switch/Assets/Scripts/Player_Move.cs
@@ -16,11 +16,15 @@ public class Player_Move : MonoBehaviour
     private bool isTouchingGround;
 
     private Animator playerAnimation;
+    public Vector3 respawnPoint; //Store position of where player is going to respawn to
 
     private void Start()  {
         //Player assets
         rigidBody = GetComponent<Rigidbody2D>();
         playerAnimation = GetComponent<Animator>();
+
+        //When game loads, respawn point is set to position of player
+        respawnPoint = transform.position;
     }
 
     private void Update() {
@@ -55,7 +59,12 @@ public class Player_Move : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)  {
         if(other.tag == "FallDetector")  {
-            //What will happen when player enters FallDetector zone
+            //Player enters FallDetector zone (collider with a trigger)
+            transform.position = respawnPoint;
+        }
+        if(other.tag == "Checkpoint") {
+            //When player reaches checkpoint, respawn point will be set to position of checkpoint
+            respawnPoint = other.transform.position;
         }
     }
 }

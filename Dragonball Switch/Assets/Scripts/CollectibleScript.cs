f980494b642082afed3511cadf81dd2af0bb3d90
tomasbrazas97 <tomasbrazas@gmail.com>
2019-03-11 17:17:00 +0000

    CoinPrefab
    Coin prefab created with trigger script

---
+++ Dragonball Switch/Assets/Scripts/CollectibleScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleScript : MonoBehaviour
+{
+    public int dragonBallScore = 0;
+    // Start is called before the first frame update
+    void Start()  {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) { 
+        Destroy(gameObject);
+        dragonBallScore += 1;
+    }
+}

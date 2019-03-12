c5db94d161609e84c9fed5f7c672401b116057a7
Jakecw97 <g00349377@gmit.ie>
2019-03-12 03:06:57 +0000

    Basic Switch Script Implemented
    Basic version of the switch script implemented. Needs tweaking to move both characters at once and  camera.
    Functions on the TAB key.

--- Dragonball Switch/Assets/Scripts/SwitchScript.cs
+++ Dragonball Switch/Assets/Scripts/SwitchScript.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class SwitchScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    //Game objects
+    public GameObject char1, char2;
+
+    //Which character is displayed
+    int charDisplayed = 1;
+
+    //Initialization 
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+        //Enable char 1 and hide char 2
+        char1.gameObject.SetActive(true);
+        char2.gameObject.SetActive(false);
+
+       
+    }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SwitchCharacter();
+        }
+    }
+    //Switch method
+     public void SwitchCharacter()
+    {
+
+        switch (charDisplayed)
+        {
+
+            // if the char 1 is displayed
+            case 1:
+
+                //switch to char 2
+                charDisplayed = 2;
+
+                // hide 1 and activate 2
+                char1.gameObject.SetActive(false);
+                char2.gameObject.SetActive(true);
+                break;
+
+            // if the char 1 is displayed
+            case 2:
+
+                //switch to char 1
+                charDisplayed = 1;
+
+                // hide 2 and activate 1
+                char1.gameObject.SetActive(true);
+                char2.gameObject.SetActive(false);
+                break;
+        }
+
     }
 }

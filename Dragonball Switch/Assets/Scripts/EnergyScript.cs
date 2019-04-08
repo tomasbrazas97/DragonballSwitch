238b3f2cf1b5f0a28f3a933e7ec36700e7561395
Jakecw97 <g00349377@gmit.ie>
2019-04-08 03:30:46 +0100

    Energy
    Energy system added. Yet to be implemented.

---
+++ Dragonball Switch/Assets/Scripts/EnergyScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnergyScript : MonoBehaviour
+{
+    private int energy;
+    private int maxEnergy = 100;
+    public event EventHandler onEnergyhange;
+
+    public EnergyScript(int maxEnergy)
+    {
+        this.maxEnergy = maxEnergy;
+        energy = maxHealth;
+    }
+
+    public int GetEnergy()
+    {
+        return energy;
+    }
+    public float GetEnergyPercent()
+    {
+        return (float)energy / maxEnergy;
+    }
+
+    public void Spend(int damageAmmount)
+    {
+        energy -= damageAmmount;
+        if (energy < 0) energy = 0;
+        if (onEnergyhange != null) onEnergyhange(this, EventArgs.Empty);
+    }
+    public void Charge(int healAmmount)
+    {
+        energy += healAmmount;
+        if (energy > maxEnergy) energy = maxEnergy;
+        if (onEnergyhange != null) onEnergyhange(this, EventArgs.Empty);
+    }
+}

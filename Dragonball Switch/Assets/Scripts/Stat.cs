a3853a32aecac81b6f930682c6db858279200b56
tomasbrazas97 <tomasbrazas@gmail.com>
2019-04-26 04:04:00 +0100

    Health bar Energy bar
    Both added, currently only working on Goku.
    Not final version

---
+++ Dragonball Switch/Assets/Scripts/Stat.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Stat 
+{
+    [SerializeField]
+    private HpBarScript bar;
+
+    [SerializeField]
+    private float maxVal;
+
+    [SerializeField]
+    private float currentVal;
+
+    public float CurrentVal
+    {
+        get
+        {
+            return currentVal;
+        }
+
+        set
+        {
+            this.currentVal = value;
+            bar.Value = currentVal;
+        }
+    }
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+
+        set
+        {
+            //Clamps, doesnt go below 0 or above MaxVal 
+            this.maxVal = Mathf.Clamp(value, 0, MaxVal);
+            bar.MaxValue = maxVal;
+        }
+    }
+
+    public void Initialize()
+    {
+        this.MaxVal = maxVal;
+        this.CurrentVal = currentVal;
+    }
+}

1a4c3f16f56c8e126f196d0eecf20d3865b1951e
Jakecw97 <g00349377@gmit.ie>
2019-04-27 15:12:12 +0100

    Camera Fix

--- Dragonball Switch/Assets/Scripts/CameraController.cs
+++ Dragonball Switch/Assets/Scripts/CameraController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
+    public GameObject player2;
     public float offset; //how far is the camera away from the player
     private Vector3 playerPosition;
+    private Vector3 player2Position;
     public float offsetTime; //time it takes for offset to gradually move into place
 
 
@@ -17,18 +19,41 @@ public class CameraController : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        //make camera follow player on x and y axis, keep it the same on z axis
-        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        if (SwitchScript.charDisplayed == 1)
+        {
+            //make camera follow player on x and y axis, keep it the same on z axis
+            playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
 
-        //adds offset so player can see infront
-        if(player.transform.localScale.x > 0f)   {
-            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
+            //adds offset so player can see infront
+            if (player.transform.localScale.x > 0f)
+            {
+                playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
+            }
+            else
+            {
+                playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
+            }
+
+            //smoothly move camera into position, deltaTime will remain smoothness no matter what computer is used
+            transform.position = Vector3.Lerp(transform.position, playerPosition, offsetTime * Time.deltaTime);
         }
         else {
-            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
-        }
+            //make camera follow player on x and y axis, keep it the same on z axis
+            player2Position = new Vector3(player2.transform.position.x, player2.transform.position.y, transform.position.z);
 
-        //smoothly move camera into position, deltaTime will remain smoothness no matter what computer is used
-        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetTime * Time.deltaTime);
+            //adds offset so player can see infront
+            if (player2.transform.localScale.x > 0f)
+            {
+                player2Position = new Vector3(player2Position.x + offset, player2Position.y, player2Position.z);
+            }
+            else
+            {
+                player2Position = new Vector3(player2Position.x - offset, player2Position.y, player2Position.z);
+            }
+
+            //smoothly move camera into position, deltaTime will remain smoothness no matter what computer is used
+            transform.position = Vector3.Lerp(transform.position, player2Position, offsetTime * Time.deltaTime);
+        }
+       
     }
 }
